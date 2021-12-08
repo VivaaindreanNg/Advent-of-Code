2f32fe7452443bd93a585dbefbe4cccc51e41824
Vivaain <vivaain@tigerlab.com>
2021-12-08 23:21:25 +0800

    Day 2 part 2

--- day2_part2.py
+++ day2_part2.py
@@ -1,7 +1,26 @@
+from utils import convert_input_file_to_list
+
 
 def solve(input_data: list) -> None:
-    return 0
+    aim = 0
+    horizontal_pos = 0
+    depth = 0
+
+    for i in input_data:
+        line = i.split()
+        direction, value = line[0], int(line[1])
+
+        if direction == 'up':
+            aim -= value
+        elif direction == 'down':
+            aim += value
+        else:
+            horizontal_pos += value
+            depth += aim * value
+
+    return horizontal_pos * depth
 
 
 if __name__ == '__main__':
-    print(solve())
+    input_data = convert_input_file_to_list('input/input2.txt')
+    print(solve(input_data))
