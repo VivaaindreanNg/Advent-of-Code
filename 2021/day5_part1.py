667829cb9f286e12399be62666154d19ca59aedc
VivaaindreanNg <123ulugubang@gmail.com>
2021-12-24 16:06:16 +0800

    Day 5 Part 1

--- 2021/day5_part1.py
+++ 2021/day5_part1.py
@@ -1,14 +1,48 @@
+from typing import List
+from utils import convert_input_file_to_list
+from collections import Counter
+
+def solve(input_data: List[str]) -> int:
+    ROW_SEPARATOR = ' -> '
+    COMMA_SEPARATOR = ','
+    mappings_dict = {}
+    coordinates = []
 
 
-def solve(input_data: str) -> int:
-    return 0
+    for i in input_data:
+        x1_y1, x2_y2 = i.split(ROW_SEPARATOR)[0], i.split(ROW_SEPARATOR)[1]
+        
+        x1_y1_tuple = tuple(map(int, x1_y1.split(COMMA_SEPARATOR)))
+        x2_y2_tuple = tuple(map(int, x2_y2.split(COMMA_SEPARATOR)))
+        
+        if x1_y1_tuple[0] == x2_y2_tuple[0] or x1_y1_tuple[1] == x2_y2_tuple[1]:
+            if x1_y1_tuple[0] == x2_y2_tuple[0]:
+                if x1_y1_tuple[1] > x2_y2_tuple[1]:
+                    for i in range(x2_y2_tuple[1], x1_y1_tuple[1]+1):
+                        coordinates.append((x1_y1_tuple[0], i))
+                else:
+                    for i in range(x1_y1_tuple[1], x2_y2_tuple[1]+1):
+                        coordinates.append((x1_y1_tuple[0], i))
+                
+            else:
+                if x1_y1_tuple[0] > x2_y2_tuple[0]:
+                    for i in range(x2_y2_tuple[0], x1_y1_tuple[0]+1):
+                        coordinates.append((i, x2_y2_tuple[1]))
+                else:
+                    for i in range(x1_y1_tuple[0], x2_y2_tuple[0]+1):
+                        coordinates.append((i, x2_y2_tuple[1]))
+
+            mappings_dict[f"{x1_y1_tuple}-{x2_y2_tuple}"] = coordinates
+        else:
+            continue
+
+    frequencies = Counter(coordinates).values()
+    mappings = list(map(lambda x: x > 1, frequencies))
+    filter_map = list(filter(lambda x: x is True, mappings))
+
+    return len(filter_map)
 
 
 if __name__ == '__main__':
-    #TODO: Read in input data from input4.txt
-
-    f = open('input/input5.txt', 'r')
-
-    for line in f.readlines():
-        print(line) 
-    print(solve(""))
+    input_data = convert_input_file_to_list('input/input5.txt')
+    print(solve(input_data))
